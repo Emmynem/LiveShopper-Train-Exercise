9d3bdfdf13a0b9364882c6490e25bb8734fdf744
Emmanuel Nwoye <emmanuelnwoye5@gmail.com>
2022-01-31 11:32:55 +0100

    Added functionalities to add different types of cars

--- Train.php
+++ Train.php
@@ -2,18 +2,25 @@
 
     class TrainCar {
         // Creating TrainCar class member variables
-        // var $type; // if you want to have different types of TrainCars i.e. cargo, engine etc
+        var $type; // if you want to have different types of TrainCars i.e. cargo, engine etc
         var $weight;
+        var $new_train_car_obj; // if you want to have different types of TrainCars i.e. cargo, engine etc
 
         // if you want to have different types of TrainCars i.e. cargo, engine etc
-        // function setType($type){ $this->type = $type; }
+        function setType($type){ $this->type = $type; }
 
         // if you want to have different types of TrainCars i.e. cargo, engine etc
-        // function getType(){ return $this->type; }
+        function getType(){ return $this->type; }
 
         function setWeight($weight){ $this->weight = $weight; }
 
         function getWeight(){ return $this->weight; }
+
+        // if you want to have different types of TrainCars i.e. cargo, engine etc
+        function setNewTrainCarObj($type, $weight) { $this->new_train_car_obj = (object) [ "type" => $type, "weight" => $weight ]; }
+
+        // if you want to have different types of TrainCars i.e. cargo, engine etc
+        function getNewTrainCarObj() { return $this->new_train_car_obj; }
     }
 
     class Train {
@@ -32,10 +39,10 @@
                 // Checking for the position where to place this current TrainCar
                 if (strtolower($position) == 'front'){
                     // Check if the train cars array is empty so as to push the first TrainCar
-                    empty($this->train_cars) ? array_push($this->train_cars, $train_car) : array_unshift($this->train_cars, $train_car);
+                    empty($this->train_cars) ? $this->train_cars[] = (object)$train_car : array_unshift($this->train_cars, (object)$train_car);
                     return true;
                 } else if (strtolower($position) == 'back') {
-                    array_push($this->train_cars, $train_car);
+                    array_push($this->train_cars, (object)$train_car);
                     return true;
                 } else {
                     return "Choose position !";
@@ -68,7 +75,7 @@
             if (empty($this->train_cars)) { echo "No train car \n"; }
             else {
                 foreach($this->train_cars as $key => $value){
-                    echo "Car ".$key." : ".$value.",\n";
+                    echo "Car ".$value->type." : ".$value->weight.",\n";
                 }
             }
         }
@@ -77,7 +84,7 @@
         function getTrainCount(){ return count($this->train_cars); }
 
         // Function to get the total weight of the train
-        function getTrainWeight(){ echo "Train total weight : \n".array_sum($this->train_cars)."\n"; }
+        function getTrainWeight(){ echo "Train total weight : \n".array_reduce($this->train_cars, function($obj, $item) { return $obj + $item->weight; })."\n"; }
 
     }
 
@@ -88,25 +95,27 @@
 
     // Step 2. Creating an object of the TrainCar class
     $trainCar1 = new TrainCar();
-    // Step 3. Setting the weight of our new TrainCar
+    $trainCar2 = new TrainCar();
+    $trainCar3 = new TrainCar();
+    // Step 3. Setting the weight and type of our new TrainCar
     $trainCar1->setWeight(30);
+    $trainCar1->setType("cargo");
+    $trainCar2->setWeight(70);
+    $trainCar2->setType("engine");
+    $trainCar3->setWeight(100);
+    $trainCar3->setType("passenger");
+    // Step 3a setting the values for our new train car object using the get methods for the type and weight
+    $trainCar1->setNewTrainCarObj($trainCar1->getType(), $trainCar1->getWeight());
+    $trainCar2->setNewTrainCarObj($trainCar2->getType(), $trainCar2->getWeight());
+    $trainCar3->setNewTrainCarObj($trainCar3->getType(), $trainCar3->getWeight());
 
     // Step 4. Add the recently created TrainCar to our Train using the addTrainCar() function, and parsing the parameters - (train_car, position)
-    $myTrain->addTrainCar($trainCar1->getWeight(), "front");
-
-    // To add more cars you can repeat from Step 2 - Step 4
-
-    // Uncomment the code below to create multiple Train Cars and add them to the Train
-    /* for($i = 0; $i < 10; $i++){
-        $randomWeight = rand(10,150);
-        $newTrainCar = new TrainCar();
-        $newTrainCar->setWeight($randomWeight);
-        $return = $myTrain->addTrainCar($newTrainCar->getWeight(), "back");
-        if($return !== true){ echo $return; break;}
-    } */
+    $myTrain->addTrainCar($trainCar1->getNewTrainCarObj(), "front");
+    $myTrain->addTrainCar($trainCar2->getNewTrainCarObj(), "back");
+    $myTrain->addTrainCar($trainCar3->getNewTrainCarObj(), "front");
 
     // Use this step to "Remove a TrainCar from either end"
-    $myTrain->removeTrainCar("front");
+    $myTrain->removeTrainCar("back");
 
     // Use this to "Ask the Train how many cars are currently in the Train"
     $myTrain->getTrainCars();
